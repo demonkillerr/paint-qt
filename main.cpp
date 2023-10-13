c34e23ecb52e8dc7f0b2b4afd49a76d8b1122bf1
demonkiller <gaurangvishwakarma619@gmail.com>
2023-10-13 12:00:25 +0530

    update project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    // The main application
+    QApplication app(argc, argv);
+
+    // Create and open the main window
+    MainWindow window;
+    window.show();
+
+    // Display the main window
+    return app.exec();
 }
